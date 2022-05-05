128182cb8fc005f479586dcaaeedb9e65b1319e9
Lorcan Mc <lorcan.jmc@gmail.com>
2022-05-05 11:20:40 +0100

    handleChange and mock data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 
 function App() {
-
-  function handleChange() {
-
+  const [text, setText] = useState("")
+console.log(text)
+  function handleChange(e) {
+    setText(e.target.value)
   }
-
 
   return (
     <div className="app">
@@ -12,12 +14,30 @@ function App() {
         <input onChange={handleChange} placeholder="Enter Location" />
       </div>
       <div className="main">
-        
+        <div className="top">
+          <h1>London</h1>
+          <h2>8°C</h2>
+          <h2>light rain</h2>
+          <h2>Feels like 6°C</h2>
+          <h2>Wind 5mph</h2>
+          <h2>Humidity 50%</h2>
+        </div>
+        <div className="bottom">
+          <div>
+            <h2>Friday</h2>
+            <p>8°C</p>
+          </div>
+          <div>
+            <h2>Saturday</h2>
+            <p>8°C</p>
+          </div>
+          <div>
+            <h2>Sunday</h2>
+            <p>8°C</p>
+          </div>
+        </div>
       </div>
-      <div className="footer">
-        
-      </div>
-      
+      <div className="footer">Made with 🤘 for 383</div>
     </div>
   );
 }
