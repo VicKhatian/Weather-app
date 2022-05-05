e574cdd04e704be58d0cd061b864b29b2e84eec9
Lorcan Mc <lorcan.jmc@gmail.com>
2022-05-05 12:37:01 +0100

    converted temperatures to celcius

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   const [text, setText] = useState("");
   const [location, setLocation] = useState("");
   const [data, setData] = useState(null);
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lat, setLat] = useState(null);
+  const [long, setLong] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,15 +13,16 @@ function App() {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
-
-      await fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=2d3e410f0ed7818625d5352a26aeb264`
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          setData(result);
-          console.log(result);
-        });
+      if (lat) {
+        await fetch(
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=2d3e410f0ed7818625d5352a26aeb264`
+        )
+          .then((res) => res.json())
+          .then((result) => {
+            setData(result);
+            console.log(result);
+          });
+      }
     };
     fetchData();
   }, [lat, long]);
@@ -40,28 +41,29 @@ function App() {
     <div className="app">
       <div className="search">
         <input onChange={handleChange} placeholder="Enter Location" />
-        <button onclick={handleClick} className="btn-weather">
+        <button onClick={handleClick} className="btn-weather">
           Submit
         </button>
       </div>
       <div className="main">
         <div className="top">
           <h1>London</h1>
-          <h2>{data.current.temp}°C</h2>
+          <h2>{Math.round(data.current.temp - 273.15)}°C</h2>
           <h2>light rain</h2>
           <h2>Feels like 6°C</h2>
           <h2>Wind 5mph</h2>
           <h2>Humidity 50%</h2>
         </div>
         <div className="bottom">
-          {/* {data.daily.map((day) => {
+          {data.daily.map((day) => {
             return (
               <div>
                 <h2>Friday</h2>
-                <p>8°C</p>
+                <p>{Math.round(day.temp.day - 273.15)}°C</p>
+                <p>{day.weather[0].description}</p>
               </div>
-            )
-          })} */}
+            );
+          })}
         </div>
       </div>
       <div className="footer">Made with 🤘 for 383</div>
